49f0b7ea4d5e439cab13b3125dad195fb11ad93d
ccalderona <ccalderona@isise.edu.pe>
2023-03-28 21:50:57 -0500

    M2

---
+++ script.js
@@ -0,0 +1,62 @@
+setTimeout(cambiaMensaje,5300)
+function cambiaMensaje(){
+    document.getElementById("loading-message").innerText = "❣Enviado❣"
+}
+setTimeout(function(){
+    alert("Ponte audifonooooooooos!!!!")
+},6000)
+
+setTimeout(function(){
+    alert("Dame next con la tecla enteeeer, porfis")
+},6100)
+
+setTimeout(function(){
+    alert("Espera que ando cargandooooo")
+},15000)
+
+setTimeout(function(){
+    alert("Espera que ando cargandooooo dije")
+},15100)
+
+setTimeout(function(){
+    alert("Bueno ya empiezo")
+},15200)
+
+setTimeout(function(){
+    alert("Tak no sé, pera")
+},15300)
+
+setTimeout(function(){
+    alert("YAYA AHORA SEE")
+},15400)
+
+setTimeout(function(){
+    alert("Naa te engané jejeje")
+},15300)
+
+setTimeout(function(){ 
+    alert("Bueno ya!")
+},15400)
+
+setTimeout(function(){
+    alert("Hoy en este día especial, quiero decirte cuán afortunado me siento de tenerte a mi lado")
+},15500)
+
+setTimeout(function(){ 
+    alert("Juntos hemos compartido momentos inolvidables que siempre guardaré en mi corazón y en mi memoria")
+},15600)
+
+
+setTimeout(function(){ 
+    alert("TE AMO Y GRACIAS POR DARLE SENTIDO A MI VIDA Y POR HACERME SENTIR QUERIDO CUANDO ESTAMOS JUNTOS")
+},15700)
+
+setTimeout(function(){ 
+    alert("Suerteeee!, que tengas un excelente día♥")
+},15800)
+
+
+ function PlayAudio(){
+ document.getElementById("musica1").play();
+    }
+
